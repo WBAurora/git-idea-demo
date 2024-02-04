eeb9ef9c229cb5c9936913589c0b52df8dda1952
Aurora <Aurora@163.com>
2024-02-04 22:45:25 +0800

    push-test2 commit

--- src/com/web/git/GitDemo.java
+++ src/com/web/git/GitDemo.java
@@ -10,5 +10,6 @@ public class GitDemo {
         System.out.println("这是hot-fix的第二版...");
         System.out.println("这是最终版...");
         System.out.println("push-text");
+        System.out.println("push-text2");
     }
 }
